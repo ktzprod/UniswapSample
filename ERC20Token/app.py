48228cd268d1c77e26a174c0b111b3dee4e01748
Quentin Despinoy <quentin.despinoy@gmail.com>
2022-04-30 11:42:59 +0200

    ERC20Token: add check_token_price_v2() that use uniswap contracts to probe token informations

--- ERC20Token/app.py
+++ ERC20Token/app.py
@@ -1,13 +1,16 @@
 import os
+import json
 
 from dotenv import load_dotenv
+from urllib.request import urlopen
 from web3 import Web3
 
 
 load_dotenv()  # take environment variables from .env.
 
-DAPP_URL=os.getenv('DAPP_URL')
+# The scope below mostly use the uniswap python API in order to easily check some informations
 
+DAPP_URL=os.getenv('DAPP_URL')
 
 token_addresses = {
     # An error may occured if you get addresses that are not checksumed (usually when using external tools).
@@ -24,7 +27,65 @@ def check_token_price(token):
     return uniswap.get_price_input(token_addresses[token], token_addresses["usdt"], 10**18)
 
 
+# The scope below focus on using the uniswap contract directly
+
+def _read_remote_abi(abi_url):
+    response = urlopen(abi_url)
+    response_content = response.read()
+    json_content = json.loads(response_content)
+    return json_content["abi"]
+
+
+UNISWAP_FACTORY_ADDRESS=os.getenv("UNISWAP_FACTORY_ADDRESS")
+UNISWAP_FACTORY_ABI_URL=os.getenv('UNISWAP_FACTORY_ABI_URL')
+UNISWAP_FACTORY_ABI=_read_remote_abi(UNISWAP_FACTORY_ABI_URL)
+
+UNISWAP_PAIR_ABI_URL=os.getenv('UNISWAP_PAIR_ABI_URL')
+UNISWAP_PAIR_ABI=_read_remote_abi(UNISWAP_PAIR_ABI_URL)
+
+UNISWAP_PAIR_ABI_URL=os.getenv('UNISWAP_PAIR_ABI_URL')
+UNISWAP_PAIR_ABI=_read_remote_abi(UNISWAP_PAIR_ABI_URL)
+
+
+def check_token_price_v2(w3_provider, token_a, token_b="usdt"):
+    # get uniswap factory contract
+    uniswap_factory = w3_provider.eth.contract(address=UNISWAP_FACTORY_ADDRESS, abi=UNISWAP_FACTORY_ABI)
+    if uniswap_factory is None:
+        raise RuntimeError("Failed to get Uniswap factory contract")
+
+    # get the contract address for the given pair [token, "usdt"]
+    pair_address = uniswap_factory.caller().getPair(token_addresses[token_a], token_addresses[token_b])
+    if pair_address is None:
+        raise RuntimeError(f"Failed to get pair contract address for pair [{token_a}, {token_b}]")
+
+    # get the uniswap pair contract for the given pair
+    uniswap_pair_contract = w3_provider.eth.contract(address=pair_address, abi=UNISWAP_PAIR_ABI)
+    if uniswap_pair_contract is None:
+        raise RuntimeError(f"Failed to get pair contract for pair [{token_a}, {token_b}]")
+
+    # get the UniswapV2ERC20 contract for each token
+    token_0_contract_address = uniswap_pair_contract.caller().token0()
+    if token_0_contract_address is None:
+        raise RuntimeError(f"Failed to get token contract address for token: {token_a}")
+
+    token_0_contract = w3_provider.eth.contract(address=token_0_contract_address, abi=UNISWAP_PAIR_ABI)
+    if token_0_contract is None:
+        raise RuntimeError(f"Failed to get token contract for token: {token_a}")
+
+    token_1_contract_address = uniswap_pair_contract.caller().token1()
+    if token_1_contract_address is None:
+        raise RuntimeError(f"Failed to get token contract address for token: {token_b}")
+
+    token_1_contract = w3_provider.eth.contract(address=token_1_contract_address, abi=UNISWAP_PAIR_ABI)
+    if token_1_contract is None:
+        raise RuntimeError(f"Failed to get token contract for token: {token_b}")
+
+    # Check that token symbols match given symbols
+    assert(token_0_contract.caller().symbol().lower() == token_a.lower())
+    assert(token_1_contract.caller().symbol().lower() == token_b.lower())
+
+
 if __name__ == "__main__":
     w3 = Web3(Web3.HTTPProvider(DAPP_URL))
     print(w3.isConnected())
-    print(check_token_price("weth"))
+    check_token_price_v2(w3, "weth")
